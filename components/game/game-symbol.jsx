9a2b93150768b5379960b614675bb783080f3158
Ddkotl <no64129@gmail.com>
2024-03-28 18:54:51 +0300

    prettier eslint

--- components/game/game-symbol.jsx
+++ components/game/game-symbol.jsx
@@ -1,14 +1,14 @@
-import { SYMBOL_O, SYMBOL_X } from './constants'
+import { SYMBOL_O, SYMBOL_X } from "./constants";
 
 export function GameSymbol({ symbol }) {
-	const getSymbolClassName = symbol => {
-		if (symbol === SYMBOL_O) return 'text-emerald-500'
-		if (symbol === SYMBOL_X) return 'text-pink-500'
-		return ''
-	}
-	return (
-		<span className={`text-xl leading-6 ${getSymbolClassName(symbol)}`}>
-			{symbol}
-		</span>
-	)
+  const getSymbolClassName = (symbol) => {
+    if (symbol === SYMBOL_O) return "text-emerald-500";
+    if (symbol === SYMBOL_X) return "text-pink-500";
+    return "";
+  };
+  return (
+    <span className={`text-xl leading-6 ${getSymbolClassName(symbol)}`}>
+      {symbol}
+    </span>
+  );
 }
